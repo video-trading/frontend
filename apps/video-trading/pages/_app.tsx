3a9cacd23e9c7f96d6e3fc5a87d8c2ac02cad60a
sirily11 <sirily1997@gmail.com>
2022-08-24 23:44:29 +0800

    fix: add website title

--- apps/video-trading/pages/_app.tsx
+++ apps/video-trading/pages/_app.tsx
@@ -3,12 +3,16 @@ import type { AppProps } from "next/app";
 import Layout from "../components/Layout";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import Head from "next/head";
 
 const queryClient = new QueryClient();
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <QueryClientProvider client={queryClient}>
+      <Head>
+        <title>Video Trading</title>
+      </Head>
       <Layout>
         <Component {...pageProps} />
       </Layout>
