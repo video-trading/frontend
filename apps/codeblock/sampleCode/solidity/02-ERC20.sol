25892b2dd0d02d9e9cfc2f94815798b6a0e6c190
sirily11 <sirily1997@gmail.com>
2022-10-25 16:21:41 +0800

    fix: update examples

---
+++ apps/codeblock/sampleCode/solidity/02-ERC20.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: GPL-3.0
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+pragma solidity >=0.7.0 <0.9.0;
+
+contract ERC20FixedSupply is ERC20 {
+    //@codeblock
+    string constant contractName = "Fixed";
+    //@codeblock
+    string constant contractSymbol = "FIX";
+    //@codeblock
+    int constant supply = 1000;
+
+    constructor() ERC20(contractName, contractSymbol) {
+        _mint(msg.sender, supply);
+    }
+}
