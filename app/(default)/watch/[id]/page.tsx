3bc512900265be12886737f478832c84a74024b5
sirily11 <sirily1997@gmail.com>
2023-05-14 23:43:49 +0800

    fix: update video

--- app/(default)/watch/[id]/page.tsx
+++ app/(default)/watch/[id]/page.tsx
@@ -1,5 +1,5 @@
+import Breadcrumbs from "@/components/shared/Breadcrumbs";
 import { PurchaseCard } from "@/components/video/PurchaseCard";
-import VideoBreadcrumb from "@/components/video/VideoBreadcrumb";
 import VideoPlayer from "@/components/video/VideoPlayer";
 import VideoReviews from "@/components/video/VideoReviews";
 import { classNames } from "@/src/classNames";
@@ -35,7 +35,20 @@ export default async function Video({ params }: any) {
       <div className="mx-auto max-w-4xl px-4 py-16 sm:px-6 sm:py-24 lg:grid lg:max-w-7xl lg:grid-cols-3 lg:gap-x-8 lg:px-8">
         {/* Product details */}
         <div className="lg:max-w-lg lg:self-end">
-          <VideoBreadcrumb category={video.Category} />
+          <Breadcrumbs
+            routes={[
+              {
+                name: video.Category.name,
+                href: `/category/${video.Category.id}`,
+                current: false,
+              },
+              {
+                name: video.title,
+                href: `/watch/${video.id}`,
+                current: true,
+              },
+            ]}
+          />
           <div className="mt-4">
             <h1 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
               {video.title}
@@ -47,51 +60,57 @@ export default async function Video({ params }: any) {
               Product information
             </h2>
 
-            <div className="flex items-center">
-              <p className="text-lg text-gray-900 sm:text-xl">
-                {video.SalesInfo?.price} HKD
-              </p>
+            {video.SalesInfo && (
+              <div className="flex items-center">
+                <p className="text-lg text-gray-900 sm:text-xl">
+                  {video.SalesInfo?.price} HKD
+                </p>
 
-              <div className="ml-4 border-l border-gray-300 pl-4">
-                <h2 className="sr-only">Reviews</h2>
-                <div className="flex items-center">
-                  <div>
-                    <div className="flex items-center">
-                      {[0, 1, 2, 3, 4].map((rating) => (
-                        <StarIcon
-                          key={rating}
-                          className={classNames(
-                            reviews.average > rating
-                              ? "text-yellow-400"
-                              : "text-gray-300",
-                            "h-5 w-5 flex-shrink-0"
-                          )}
-                          aria-hidden="true"
-                        />
-                      ))}
+                <div className="ml-4 border-l border-gray-300 pl-4">
+                  <h2 className="sr-only">Reviews</h2>
+                  <div className="flex items-center">
+                    <div>
+                      <div className="flex items-center">
+                        {[0, 1, 2, 3, 4].map((rating) => (
+                          <StarIcon
+                            key={rating}
+                            className={classNames(
+                              reviews.average > rating
+                                ? "text-yellow-400"
+                                : "text-gray-300",
+                              "h-5 w-5 flex-shrink-0"
+                            )}
+                            aria-hidden="true"
+                          />
+                        ))}
+                      </div>
+                      <p className="sr-only">
+                        {reviews.average} out of 5 stars
+                      </p>
                     </div>
-                    <p className="sr-only">{reviews.average} out of 5 stars</p>
+                    <p className="ml-2 text-sm text-gray-500">
+                      {reviews.totalCount} reviews
+                    </p>
                   </div>
-                  <p className="ml-2 text-sm text-gray-500">
-                    {reviews.totalCount} reviews
-                  </p>
                 </div>
               </div>
-            </div>
+            )}
 
             <div className="mt-4 space-y-6">
               <Editor initialValue={video.description} editable={false} />
             </div>
 
-            <div className="mt-6 flex items-center">
-              <CheckIcon
-                className="h-5 w-5 flex-shrink-0 text-green-500"
-                aria-hidden="true"
-              />
-              <p className="ml-2 text-sm text-gray-500">
-                Ready to be purchased
-              </p>
-            </div>
+            {video.SalesInfo && (
+              <div className="mt-6 flex items-center">
+                <CheckIcon
+                  className="h-5 w-5 flex-shrink-0 text-green-500"
+                  aria-hidden="true"
+                />
+                <p className="ml-2 text-sm text-gray-500">
+                  Ready to be purchased
+                </p>
+              </div>
+            )}
           </section>
         </div>
 
@@ -105,22 +124,24 @@ export default async function Video({ params }: any) {
         </div>
 
         {/* Product form */}
-        <div className="mt-10 lg:col-start-1 lg:row-start-2 lg:max-w-lg lg:self-start">
-          <PurchaseCard
-            videoId={video.id}
-            purchaseOptions={[
-              {
-                name: t("purchase-option-1"),
-                description: t("purchase-option-1-description"),
-              },
-              {
-                name: t("purchase-option-2"),
-                description: t("purchase-option-2-description"),
-                disabled: true,
-              },
-            ]}
-          />
-        </div>
+        {video.SalesInfo && (
+          <div className="mt-10 lg:col-start-1 lg:row-start-2 lg:max-w-lg lg:self-start">
+            <PurchaseCard
+              videoId={video.id}
+              purchaseOptions={[
+                {
+                  name: t("purchase-option-1"),
+                  description: t("purchase-option-1-description"),
+                },
+                {
+                  name: t("purchase-option-2"),
+                  description: t("purchase-option-2-description"),
+                  disabled: true,
+                },
+              ]}
+            />
+          </div>
+        )}
         <div className="col-span-4">
           <VideoReviews />
         </div>
