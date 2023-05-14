45c917379e5e244ae5ea6dae266ff46e5aaf538e
sirily11 <sirily1997@gmail.com>
2023-05-14 23:37:24 +0800

    fix: checkout page layout

---
+++ app/checkout/[id]/loading.tsx
@@ -0,0 +1,6 @@
+import { CircularProgressBarFullscreen } from "@/components/shared/Placeholders";
+import React from "react";
+
+export default function loading() {
+  return <CircularProgressBarFullscreen />;
+}
