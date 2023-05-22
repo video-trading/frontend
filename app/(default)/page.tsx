27883be9b055b469b6e48d49a9390c224e420fa3
sirily11 <sirily1997@gmail.com>
2023-05-22 15:24:36 +0800

    fix: redirect error

--- app/(default)/page.tsx
+++ app/(default)/page.tsx
@@ -3,20 +3,11 @@ export const metadata = {
   description: "Page description",
 };
 
-import Hero from "@/components/home/hero";
-import Inspiration from "@/components/home/inspiration";
-import Carousel from "@/components/home/carousel";
-import Creatives from "@/components/home/creatives";
-import Pricing from "@/components/home/pricing";
-import Testimonials from "@/components/home/testimonials";
-import Faqs from "@/components/home/faqs";
-import Blog from "@/components/home/blog";
-import Cta from "@/components/home/cta";
-import { CategoryService } from "@/src/services/CategoryService";
-import { VideoService } from "@/src/services/VideoService";
-import { getServerSession } from "next-auth";
-import { authOptions } from "@/src/authOptions";
 import CategoryDisplay from "@/components/home/CategoryDisplay";
+import Creatives from "@/components/home/creatives";
+import Cta from "@/components/home/cta";
+import Hero from "@/components/home/hero";
+import Testimonials from "@/components/home/testimonials";
 import VideoDisplay from "@/components/home/VideoDisplay";
 
 export default async function Home() {
