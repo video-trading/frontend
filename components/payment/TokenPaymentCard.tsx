0e8f4d5ebd50738be4ee6c96643e87a3fee0f951
sirily11 <32106111+sirily11@users.noreply.github.com>
2023-05-28 18:01:15 +0800

    fix: add mfa to checkout page (#49)
    * fix: add webauthn support
    
    * fix: add mfa description

--- components/payment/TokenPaymentCard.tsx
+++ components/payment/TokenPaymentCard.tsx
@@ -6,13 +6,19 @@ import { useRouter } from "next/navigation";
 import React, { useCallback } from "react";
 import ContainedButton from "../shared/ContainedButton";
 import LoadingButton from "../shared/LoadingButton";
+import { AuthenticationService } from "@/src/services/AuthenticationService";
 
 interface Props {
+  username: string;
   accessToken: string;
   videoId: string;
 }
 
-export default function TokenPaymentCard({ accessToken, videoId }: Props) {
+export default function TokenPaymentCard({
+  accessToken,
+  videoId,
+  username,
+}: Props) {
   const { t } = useTranslation("tx");
   const [loading, setLoading] = React.useState(false);
   const router = useRouter();
@@ -20,6 +26,11 @@ export default function TokenPaymentCard({ accessToken, videoId }: Props) {
   const onClick = useCallback(async () => {
     try {
       setLoading(true);
+      await AuthenticationService.mfaVerification({
+        accessKey: accessToken,
+        username,
+      });
+
       const result = await PaymentService.checkoutWithToken(
         accessToken,
         videoId
@@ -33,13 +44,18 @@ export default function TokenPaymentCard({ accessToken, videoId }: Props) {
   }, [accessToken, videoId]);
 
   return (
-    <LoadingButton
-      onClick={onClick}
-      loading={loading}
-      disabled={loading}
-      className="w-full rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 mt-1"
-    >
-      {t("pay-with-token")}
-    </LoadingButton>
+    <div className="flex-row">
+      <LoadingButton
+        onClick={onClick}
+        loading={loading}
+        disabled={loading}
+        className="w-full rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 mt-1"
+      >
+        {t("pay-with-token")}
+      </LoadingButton>
+      <div>
+        <p className="text-red-600">{t("require-mfa-desc")}</p>
+      </div>
+    </div>
   );
 }
--- src/services/AuthenticationService.ts
+++ src/services/AuthenticationService.ts
@@ -1,4 +1,7 @@
 import axios from "axios";
+import { client } from "@passwordless-id/webauthn";
+import { z } from "zod";
+import { parseAuthentication } from "@passwordless-id/webauthn/dist/esm/parsers";
 
 interface LoginResponse {
   user: Profile;
@@ -21,6 +24,39 @@ export interface Profile {
     address: string;
   };
 }
+
+const RegisterMfaAuthenticationParams = z.object({
+  accessKey: z.string(),
+  username: z.string({ description: "username" }),
+});
+
+const RegisterMfaAuthenticationReturn = z.enum([
+  "registered",
+  "already_registered",
+  "error",
+  "not_supported",
+]);
+
+type RegisterMfaAuthenticationReturn = z.infer<
+  typeof RegisterMfaAuthenticationReturn
+>;
+
+const GetMfaAuthenticationResponse = z.object({
+  id: z.string().optional(),
+  challenge: z.string(),
+  status: z.enum(["registered", "not_registered"], {
+    description: "Status of the MFA device registration on the server",
+  }),
+});
+
+const CreateMfaAuthenticationResponse = z.object({
+  id: z.string(),
+});
+
+const VerifyMfaAuthenticationResponse = z.object({
+  challenge: z.string(),
+  id: z.string(),
+});
 
 export class AuthenticationService {
   static async signIn(
@@ -76,5 +112,116 @@ export class AuthenticationService {
       }
     );
     return profile.data;
+  }
+
+  /**
+   * Register a new MFA device
+   */
+  static async mfaVerification({
+    accessKey,
+    username,
+  }: z.infer<
+    typeof RegisterMfaAuthenticationParams
+  >): Promise<RegisterMfaAuthenticationReturn> {
+    // Check if the browser supports WebAuthn
+    if (!client.isAvailable()) {
+      return "not_supported";
+    }
+    // access GET /auth/mfa/register to get the challenge
+    const response = await axios.get(
+      process.env.NEXT_PUBLIC_API_ENDPOINT + "/auth/mfa/register",
+      {
+        headers: {
+          Authorization: `Bearer ${accessKey}`,
+        },
+      }
+    );
+
+    let { challenge, status } = GetMfaAuthenticationResponse.parse(
+      response.data
+    );
+
+    console.log(status);
+
+    if (status === "not_registered") {
+      await AuthenticationService.registerMfaDevice({
+        username,
+        challenge,
+        accessKey,
+      });
+    }
+
+    await AuthenticationService.verifyMfa({ accessKey });
+    return "registered";
+  }
+
+  /**
+   * Register a new MFA device.
+   * Do not call this method directly, use `mfaVerification` instead.
+   */
+  static async registerMfaDevice({
+    username,
+    challenge,
+    accessKey,
+  }: {
+    username: string;
+    challenge: string;
+    accessKey: string;
+  }) {
+    const encoded = await client.register(username, challenge);
+    try {
+      const response = await axios.post(
+        process.env.NEXT_PUBLIC_API_ENDPOINT + "/auth/mfa/register",
+        encoded,
+        {
+          headers: {
+            Authorization: `Bearer ${accessKey}`,
+          },
+        }
+      );
+      return;
+    } catch (e: any) {
+      // check if the error is axios error
+      if (e.response?.data?.message) {
+        console.log(e.response.data);
+        throw new Error(e.response.data.message);
+      }
+      throw e;
+    }
+  }
+
+  /**
+   * Do not call this method directly, use `mfaVerification` instead.
+   * @param param0
+   */
+  static async verifyMfa({ accessKey }: { accessKey: string }) {
+    const response = await axios.get(
+      process.env.NEXT_PUBLIC_API_ENDPOINT + "/auth/mfa/authenticate",
+      {
+        headers: {
+          Authorization: `Bearer ${accessKey}`,
+        },
+      }
+    );
+
+    const { challenge, id } = VerifyMfaAuthenticationResponse.parse(
+      response.data
+    );
+
+    const authentication = await client.authenticate([id], challenge, {
+      authenticatorType: "auto",
+      userVerification: "required",
+      timeout: 60000,
+    });
+
+    await axios.post(
+      process.env.NEXT_PUBLIC_API_ENDPOINT + "/auth/mfa/authenticate",
+      authentication,
+      {
+        headers: {
+          Authorization: `Bearer ${accessKey}`,
+        },
+      }
+    );
   }
 }
