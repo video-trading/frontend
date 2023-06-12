1f673b4c232b3508d94d96a0ddb492ac395a97bf
sirily11 <32106111+sirily11@users.noreply.github.com>
2023-06-12 13:11:00 +0800

    fix: add auto Check-links function (#53)
    * fix: add playwright tests
    
    * fix: add auto tests

---
+++ tests/link.spec.ts
@@ -0,0 +1,34 @@
+import { test, expect, Page } from "@playwright/test";
+
+const url = "https://vxvhk.com";
+
+async function checkLinks(page: Page, link: string, visited: string[]) {
+  // get all links
+  // check if link is valid
+
+  await page.goto(link);
+  await page.waitForLoadState("networkidle");
+  const links = await page.$$eval("a", (links) =>
+    links.map((link) => link.getAttribute("href"))
+  );
+  //@ts-expect-error
+  let filteredLinks: string[] = links.filter((l) => l !== null);
+  filteredLinks = filteredLinks
+    .filter((l) => !l.startsWith("https"))
+    .filter((l) => !l.startsWith("#"))
+    .filter((l) => !visited.includes(l))
+    .filter((l) => l !== "/");
+
+  for (const link of filteredLinks) {
+    if (visited.includes(link)) continue;
+    const url = new URL(link, page.url());
+    visited.push(link);
+    await checkLinks(page, url.href, visited);
+  }
+}
+
+test("Check every link", async ({ page }) => {
+  test.setTimeout(300_000);
+  const visited: string[] = [];
+  await checkLinks(page, url, visited);
+});
